5a2d05bccd51dbd377a7f864f08d202272d9f160
antonM1234 <ant20180635@std.psut.edu.jo>
2021-05-23 11:58:02 +0200

    minor changes

---
+++ sspAssignment3/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sspAssignment3.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult nurseDetails()
+        {
+            return View();
+        }
+
+        public ViewResult createdPage()
+        {
+            return View();
+        }
+
+        public ViewResult submitForm()
+        {
+            return View();
+        }
+    }
+}
