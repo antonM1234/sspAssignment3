9eeb9ef776ab948541e502ebf77196ea8fac43b6
antonM1234 <ant20180635@std.psut.edu.jo>
2021-05-23 12:27:44 +0200

    added models, added classes and methods, update/delete/add for nurses

---
+++ sspAssignment3/Models/INurseRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sspAssignment3.Models
+{
+    public interface INurseRepository
+    {
+        public IEnumerable<Nurse> ShowAllNurses();
+
+        public Nurse GetNurse(int ID);
+
+        public Nurse AddNurse(Nurse n);
+
+        public Nurse DeleteNurse(int ID);
+
+        public Nurse UpdateNurse(Nurse n);
+
+    }
+}
---
+++ sspAssignment3/Models/NurseSQLRepository.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sspAssignment3.Models
+{
+    public class NurseSQLRepository : INurseRepository
+    {
+        private IEnumerable<Nurse> nurses;
+
+        public NurseSQLRepository()
+        {
+            nurses = new List<Nurse>()
+            {
+                new Nurse() { ID= 1, Name="aaa", Email="aaa@a.com", Section="first"},
+                new Nurse() { ID= 2, Name="bbb", Email="bbb@a.com", Section="Second"},
+                new Nurse() { ID= 3, Name="ccc", Email="ccc@a.com", Section="Third"}
+            };
+        }
+
+        public Nurse GetNurse(int ID)
+        {
+            return nurses.Where(e => e.ID == ID).FirstOrDefault();
+        }
+
+        public IEnumerable<Nurse> ShowAllNurses()
+        {
+            return nurses;
+        }
+
+        //Add(Nurse n), Delete(int id) and Update(Nurse n) methods:
+
+        public Nurse AddNurse(Nurse n) 
+        {
+            n.ID = nurses.Max(e => e.ID) + 1;
+            ((List<Nurse>)nurses).Add(n);
+            return n;
+        }
+        public Nurse DeleteNurse(int ID)
+        {
+            Nurse n = nurses.FirstOrDefault(e => e.ID == ID);
+            if(n != null)
+            {
+                ((List<Nurse>)nurses).Remove(n);
+            }
+            return n;
+        }
+
+        public Nurse UpdateNurse(Nurse n)
+        {
+            Nurse nTemp = nurses.FirstOrDefault(e => e.ID == n.ID);
+            if (nTemp != null)
+            {
+                nTemp.Name = n.Name;
+                nTemp.Email = n.Email;
+                nTemp.Section = n.Section;
+            }
+            return nTemp;
+        }
+
+    }
+}
