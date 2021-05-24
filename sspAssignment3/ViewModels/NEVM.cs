ce409ba421580b713e363ca3a02883153e8621ca
Waleed Kamal <wal20180078@std.psut.edu.jo>
2021-05-24 17:30:09 +0300

    Path fixes

---
+++ sspAssignment3/ViewModels/NEVM.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace sspAssignment3.ViewModels
+{
+    public class NEVM : NCVM
+    {
+        public int ID { get; set; }
+
+        public string ExistingPhoto { get; set; }
+    }
+}
